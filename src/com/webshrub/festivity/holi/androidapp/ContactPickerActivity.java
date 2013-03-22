41a89a1cf6cc12cc113d8c8fcb7b829e5f25ed00
ahsan_cse2004 <ahsan_cse2004@6259ad73-3fe8-4e07-8c0f-84a7e7eb6c4c>
2013-03-22 13:58:03 +0000

    fixed home button - Ahsan
    git-svn-id: https://subversion.assembla.com/svn/festivity/trunk@69 6259ad73-3fe8-4e07-8c0f-84a7e7eb6c4c

--- src/com/webshrub/festivity/holi/androidapp/ContactPickerActivity.java
+++ src/com/webshrub/festivity/holi/androidapp/ContactPickerActivity.java
@@ -1,8 +1,10 @@
 package com.webshrub.festivity.holi.androidapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.MenuItem;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,12 +17,27 @@ public class ContactPickerActivity extends SherlockFragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         FragmentManager fragmentManager = getSupportFragmentManager();
         // Create the list fragment and add it as our sole content.
         if (fragmentManager.findFragmentById(android.R.id.content) == null) {
             ContactPickerListFragment contactPickerListFragment = new ContactPickerListFragment();
             contactPickerListFragment.setArguments(getIntent().getExtras());
             fragmentManager.beginTransaction().add(android.R.id.content, contactPickerListFragment).commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent();
+                intent.setClass(this, FestivityItemDetailsActivity.class);
+                intent.putExtra(FestivityConstants.FESTIVITY_ITEM, getIntent().getExtras().getParcelable(FestivityConstants.FESTIVITY_ITEM));
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
